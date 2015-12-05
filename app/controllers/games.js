276c9b9cf8433781596d9a04659de5a0271636bb
aarnwri <aarnwri@gmail.com>
2015-12-04 21:14:18 -0800

    refactored games route for consistency with other routes

--- app/controllers/games.js
+++ app/controllers/games.js
@@ -1,50 +1,14 @@
 import Ember from 'ember';
-import ModalForm from '../mixins/modal-form';
 
-export default Ember.Controller.extend(ModalForm, {
+export default Ember.Controller.extend({
 
   games: '',
-  // newGameName: '',  // used for the create-game modal
 
   actions: {
-    createGame: function () {
-      var game = this.store.createRecord('game');
-      game.save().then(function (game) {
-        // success callback
-        console.log("game created successfully: " + game);
-
-      }, function (err) {
-        // failure callback
-        console.log("error creating game: err: " + err);
-
-      }).finally(function () {
-        // controller.set('newGameName', '');
-      });
-    },
-
     joinGame: function () {
-      console.log('joinGame called, but not implemented yet...');
-    },
+      // TODO: I think we need a form to fill out... maybe a join game controller...
 
-    // called by the create-game modal
-    // saveGame: function () {
-    //   console.log('saveGame called');
-    //   // TODO: process any validations in completing this form... name should be less than 50 chars
-    //
-    //   var controller = this;
-    //
-    //   var game = this.store.createRecord('game', { name: this.get('newGameName') });
-    //   game.save().then(function (game) {
-    //     // success callback
-    //     console.log("game created successfully: " + game);
-    //
-    //   }, function (err) {
-    //     // failure callback
-    //     console.log("error creating game: err: " + err);
-    //
-    //   }).finally(function () {
-    //     controller.set('newGameName', '');
-    //   });
-    // }
+      console.log('joinGame called, but not implemented yet...');
+    }
   }
 });
--- app/routes/games.js
+++ app/routes/games.js
@@ -8,6 +8,31 @@ export default Ember.Route.extend({
 
   setupController: function (controller, model) {
     controller.set('games', model);
+  },
+
+  createGame: function () {
+    var game = this.store.createRecord('game');
+    game.save().then(
+      this.onCreateGameSuccess.bind(this),
+      this.onCreateGameFail.bind(game)
+    );
+  },
+
+  onCreateGameSuccess: function (game) {
+    this.transitionTo('games.game', game);
+    console.log("game created successfully: " + game);
+  },
+
+  onCreateGameFail: function (error) {
+    // this bound to game object
+    this.removeSelf();
+    console.log("saving game failed: err: " + error);
+  },
+
+  actions: {
+    submitCreateGame: function () {
+      this.createGame();
+    }
   }
 
 });
